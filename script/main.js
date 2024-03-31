4b502966acb0ebd2253aa9f2b932926dee0c7731
Nathannn <144454077+Nthnnn26@users.noreply.github.com>
2024-03-31 12:59:47 +0800

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,34 @@
+function homePage() {
+    window.location.href = 'home.html';
+}
+
+function index2() {
+    window.location.href = 'index-2.html';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    setTimeout(function() {
+        document.body.style.display = 'block';
+    }, 2000); 
+});
+
+
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+        
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
